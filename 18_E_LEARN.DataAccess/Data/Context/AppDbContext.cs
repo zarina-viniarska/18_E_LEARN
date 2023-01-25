d5f0e37a04ffe18413826c75e501d55ed808b58b
trofimchuk <trofimchuk@itstep.academy>
2023-01-25 19:22:06 +0200

    Add project files.

---
+++ 18_E_LEARN.DataAccess/Data/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_E_LEARN.DataAccess.Data.Context
+{
+    public class AppDbContext : IdentityDbContext
+
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connectionStrings = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionStrings);
+
+        }
+    }
+}
---
+++ 18_E_LEARN.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _18_E_LEARN.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
