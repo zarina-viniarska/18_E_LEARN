0b3718478ad93c3fd786f0c432e78efd121789d9
trofimchuk <trofimchuk@itstep.academy>
2023-01-30 18:55:42 +0200

    Start with Dashboard

---
+++ 18_E_LEARN.Web/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _18_E_LEARN.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
