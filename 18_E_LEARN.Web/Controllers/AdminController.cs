7bd2e29f36611f32b84a92e1a6665a1f97347e8d
trofimchuk <trofimchuk@itstep.academy>
2023-02-01 19:22:23 +0200

    Start with login

--- 18_E_LEARN.Web/Controllers/AdminController.cs
+++ 18_E_LEARN.Web/Controllers/AdminController.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _18_E_LEARN.DataAccess.Data.Models.User;
+using _18_E_LEARN.DataAccess.Data.ViewModels.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace _18_E_LEARN.Web.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AdminController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -19,11 +33,42 @@ namespace _18_E_LEARN.Web.Controllers
             return View();
         }
 
+        [AllowAnonymous]
+        [HttpGet]
         public IActionResult SignIn()
         {
             return View();
         }
 
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> SignIn(LoginUserVM model)
+        {
+            var user = await  _userManager.FindByEmailAsync(model.Email);
+
+            if (user == null)
+            {
+                return View("User not found!");
+            }
+            else
+            {
+
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(model);
+                }
+            }
+
+           
+        }
+
+        [AllowAnonymous]
         public IActionResult SignUp()
         {
             return View();
